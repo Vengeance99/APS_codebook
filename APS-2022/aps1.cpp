76eec2716c018d31228fc381d53c289ec3701f57
Vengeance99 <shivanandkundargi992@gmail.com>
2022-02-01 15:11:58 +0530

    init

---
+++ APS-2022/aps1.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+void sort(int a[],int n)
+{
+    int i,j,tmp=0;
+    for(i=0;i<n;i++)
+    {  
+        for(j=0;j<n;j++)
+        { 
+          if(a[j]>a[j+1])
+          {
+               tmp=a[j];
+               a[j]=a[j+1];
+               a[j+1]=tmp;
+          }
+        }
+    }
+}
+int main()
+{
+    int n,i,a[100];
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    sort(a,n);
+    for(i=0;i<n;i++)
+    {
+        cout<<a[i]<<" "<<endl;
+        
+    }
+    return 0;
+    cout<<"aps";
+}
+
+
+
